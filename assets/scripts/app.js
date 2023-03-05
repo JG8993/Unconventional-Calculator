117c8a0c450ec60391568aff7e2a922cba87dfa3
Jacob <jjgomez1989@yahoo.com>
2023-03-04 16:00:57 -0800

    Unconvential Calcualtor

---
+++ assets/scripts/app.js
@@ -0,0 +1,60 @@
+const defaultResult = 0;
+let currentResult = defaultResult;
+let logEntries = [];
+
+function getUserNumberInput() {
+  return parseInt(userInput.value);
+}
+
+function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
+  const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
+  outputResult(currentResult, calcDescription);
+}
+
+function writeToLog(operationID, prevResult, operationNumUsed, newResult) {
+  const logEntry = {
+    operation: operationID,
+    prevResult: prevResult,
+    numberAdded: operationNumUsed,
+    newResult: newResult,
+  };
+  logEntries.push(logEntry);
+  console.log(logEntries);
+}
+
+function add() {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult += enteredNumber;
+  createAndWriteOutput("+", initialResult, enteredNumber);
+  writeToLog("Add", initialResult, enteredNumber, currentResult);
+}
+
+function subtract() {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult -= enteredNumber;
+  createAndWriteOutput("-", initialResult, enteredNumber);
+  writeToLog("Subtract", initialResult, enteredNumber, currentResult);
+}
+
+function multiply() {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult *= enteredNumber;
+  createAndWriteOutput("*", initialResult, enteredNumber);
+  writeToLog("Multiply", initialResult, enteredNumber, currentResult);
+}
+
+function divide() {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult /= enteredNumber;
+  createAndWriteOutput("/", initialResult, enteredNumber);
+  writeToLog("Divide", initialResult, enteredNumber, currentResult);
+}
+
+addBtn.addEventListener("click", add);
+subtractBtn.addEventListener("click", subtract);
+multiplyBtn.addEventListener("click", multiply);
+divideBtn.addEventListener("click", divide);
---
+++ assets/scripts/tempCodeRunnerFile.js
@@ -0,0 +1,3 @@
+let calculationDescription = `(${defaultResult} + 10) * 3/2 -1`;
+
+// outputResult(currentResult, calculationDescription);
